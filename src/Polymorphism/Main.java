b4d2e3a8589d54aff777a37c4d301bef2df435d1
sukant43 <sukant98657@gmail.com>
2025-04-20 21:45:20 +0530

    polymorphism finished

---
+++ src/Polymorphism/Main.java
@@ -0,0 +1,10 @@
+package Polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Shapes shape=new Shapes();
+        Shapes circle=new Circle();
+        Shapes square=new Square();
+        square.area();
+    }
+}
