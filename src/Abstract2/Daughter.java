b37c1ef4d7e4b342df7c2dd42f62406bc51d3d42
sukant43 <sukant98657@gmail.com>
2025-04-22 23:25:27 +0530

    abstract class

---
+++ src/Abstract2/Daughter.java
@@ -0,0 +1,10 @@
+package Abstract2;
+
+public class Daughter extends Parent{
+    void career(String name){
+        System.out.println("Doctor");
+    }
+    void partner(String name,int age){
+        System.out.println("Do Know");
+    }
+}
--- src/Abstract2/Son.java
+++ src/Abstract2/Son.java
@@ -3,7 +3,7 @@ package Abstract2;
 public class Son extends Parent {
     @Override
     void career(String name) {
-        System.out.println("I am a " + name);
+        System.out.println("I want to become" + name);
     }
 
     @Override
