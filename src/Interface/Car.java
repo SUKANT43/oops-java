ec33a649b65ef0dd0a2df488a6d4c7a89342b0a8
sukant43 <sukant98657@gmail.com>
2025-04-27 13:15:10 +0530

    interfaces ongoinf

---
+++ src/Interface/Car.java
@@ -0,0 +1,23 @@
+package Interface;
+
+public class Car implements Brake,Engine{
+    @Override
+    public void brake(){
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void acc() {
+
+    }
+}
---
+++ src/Interface/Engine.java
@@ -0,0 +1,9 @@
+package Interface;
+
+public interface Engine {
+    static final int price=80000;
+    void start();
+    void stop();
+    void acc();
+
+}
