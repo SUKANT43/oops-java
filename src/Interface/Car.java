8c2a1f0b7a90ec750aa074ccf336748fa80856be
sukant43 <sukant98657@gmail.com>
2025-04-27 13:18:50 +0530

    interfaces ongoing

--- src/Interface/Car.java
+++ src/Interface/Car.java
@@ -3,21 +3,21 @@ package Interface;
 public class Car implements Brake,Engine{
     @Override
     public void brake(){
-
+        System.out.println("I brake a like a normal car");
     }
 
     @Override
     public void start(){
-
+        System.out.println("I start a like a normal car");
     }
 
     @Override
     public void stop() {
-
+        System.out.println("I stop a like a normal car");
     }
 
     @Override
     public void acc() {
-
+        System.out.println("I acc a like a normal car");
     }
 }
--- src/Interface/Main.java
+++ src/Interface/Main.java
@@ -2,6 +2,9 @@ package Interface;
 
 public class Main {
     public static void main(String[] args) {
-
+        Car car=new Car();
+        car.start();
+        car.acc();
+        car.stop();
     }
 }
